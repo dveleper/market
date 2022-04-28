f1e286e0c686b4c48031d6e768a195a8e942482c
Diego Velez <diavelez@bancolombia.com.co>
2022-04-27 22:08:28 -0500

    implementation mapstruct on domain for product and category

--- src/main/java/com/platzi/market/persistence/entity/Producto.java
+++ src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -17,27 +17,19 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_producto")
     private Integer idProducto;
-
     private String nombre;
-
     @Column(name = "id_categoria")
     private Integer idCategoria;
-
     @Column(name = "codigo_barras")
     private String codigoBarras;
-
     @Column(name = "precio_venta")
     private Double precioVenta;
-
     @Column(name = "cantidad_stock")
     private Integer cantidadStock;
-
     private Boolean estado;
-
     @ManyToOne
     @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
     private Categoria categoria;
-
     public Integer getIdProducto() {
         return idProducto;
     }
