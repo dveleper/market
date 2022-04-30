b5e18bedb01295e978785d88bd07f004f616d0fe
Diego Velez <diavelez@bancolombia.com.co>
2022-04-30 15:05:25 -0500

    implementation method that generates json web token

---
+++ src/main/java/com/platzi/market/domain/dto/AuthenticationRespose.java
@@ -0,0 +1,17 @@
+package com.platzi.market.domain.dto;
+
+public class AuthenticationRespose {
+    private String jwt;
+
+    public AuthenticationRespose(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
---
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -0,0 +1,27 @@
+package com.platzi.market.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JWTUtil {
+    private static String KEY = "pl4tz1";
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
